786b6d5a265316e568be6a9a33e70486ac2b9d68
Aaron Kling <webgeek1234@gmail.com>
2015-04-21 21:56:41 -0500

    With this, gk20a should finally be happy and allow booting to internal and secondaries normally.

--- multirom/mr_hooks.c
+++ multirom/mr_hooks.c
@@ -6,7 +6,9 @@
 #include <sys/mount.h>
 #include <pthread.h>
 #include <fcntl.h>
+#include <util.h>
 
+#if MR_DEVICE_HOOKS >= 1
 #define PID_TOUCH    "/touch.pid"
 #define PID_WATCHDOG "/watchdog.pid"
 
@@ -27,15 +29,69 @@ static void kill_pid(const char* path) {
 	unlink(path);
 }
 
+void mrom_hook_before_fb_close() {
+	// Kill the extra threads
+	kill_pid(PID_TOUCH);
+	kill_pid(PID_WATCHDOG);
+}
+
+int mrom_hook_after_android_mounts(__attribute__((unused))const char *busybox_path, __attribute__((unused))const char *base_path, __attribute__((unused))int type) {
+	mrom_hook_before_fb_close();
+	return 0;
+}
+#endif
+
+#if MR_DEVICE_HOOKS >= 3
+// If gk20a doesn't get firmware, boot to secondary is slow and the internal fails to init graphics.
+// The symlink is needed for an automatic followup firmware load.
+// There has got to be a simpler way to do this.
+static void load_firmware() {
+	int fd_loading = -1, c = 0;
+	FILE *fd_firm = NULL, *fd_data = NULL;
+
+	// Mount internal /system to /sysinit. Mounting to /system causes problems
+	mkdir("/sysint", 0755);
+	wait_for_file("/dev/block/platform/sdhci-tegra.3/by-name/APP", 10);
+	mount("/dev/block/platform/sdhci-tegra.3/by-name/APP", "/sysint", "ext4", MS_RDONLY, NULL);
+	symlink("/sysint/etc", "/etc");
+
+	// Open the device for firmware loading
+	wait_for_file("/sys/class/firmware/gk20a!gpmu_ucode.bin/loading", 10);
+	fd_loading = open("/sys/class/firmware/gk20a!gpmu_ucode.bin/loading", O_WRONLY);
+	write(fd_loading, "1", 1);
+
+	// Copy the firmware into the device
+	wait_for_file("/sysint/etc/firmware/gk20a/gpmu_ucode.bin", 10);
+	fd_firm = fopen("/sysint/etc/firmware/gk20a/gpmu_ucode.bin", "rb");
+	fd_data = fopen("/sys/class/firmware/gk20a!gpmu_ucode.bin/data", "wb");
+	while((c=getc(fd_firm))!=EOF)
+		fprintf(fd_data,"%c",c);
+	fclose(fd_firm);
+	fclose(fd_data);
+
+	// Close the device firmware loader
+	write(fd_loading, "0", 1);
+	close(fd_loading);
+
+	// Here, gk20a loads another firmware blob from /etc/firmware
+	// Arbitrarily wait a second for it to finish loading and unmount everything
+	sleep(1);
+	unlink("/etc");
+	umount("/sysint");
+	unlink("/sysint");
+	ERROR("Finished gk20a firmware load.\n");
+}
+
 // Touch thread. Touchscreen driver wrapper needs run separately.
 static void ts_thread() {
-	symlink("/realdata/media/0/multirom/touchscreen", "/sbin/touchscreen");
 	wait_for_file("/dev/touch", 10);
 	wait_for_file("/realdata/media/0/multirom", 10);
+	symlink("/realdata/media/0/multirom/touchscreen", "/sbin/touchscreen");
 
 	ERROR("Starting rm-wrapper...\n");
 	char* argv[] = {"/sbin/touchscreen/rm-wrapper", NULL};
 	char* envp[] = {"LD_LIBRARY_PATH=/sbin/touchscreen", NULL};
+	write_pid(PID_TOUCH, getpid());
 	execve(argv[0], &argv[0], envp);
 }
 
@@ -54,60 +110,38 @@ static void wd_thread() {
 	}
 }
 
-// If gk20a doesn't get firmware, boot to secondary is slow and the internal fails to init graphics.
-// The symlink is needed for an automatic followup firmware load.
-// There has got to be a simpler way to do this.
-static void load_firmware() {
-	int fd_loading = -1, c = 0;
-	FILE *fd_firm = NULL, *fd_data = NULL;
-	
-	while ((fd_loading = open("/sys/class/firmware/gk20a!gpmu_ucode.bin/loading", O_WRONLY)) == -1)
-		sleep(1);
-	write(fd_loading, "1", 1);
-
-	wait_for_file("/dev/block/platform/sdhci-tegra.3/by-name/APP", 10);
-	mount("/dev/block/platform/sdhci-tegra.3/by-name/APP", "/system", "ext4", MS_RDONLY, NULL);
-	symlink("/system/etc", "/etc");
-
-	wait_for_file("/system/etc/firmware/gk20a/gpmu_ucode.bin", 10);
-	fd_firm = fopen("/system/etc/firmware/gk20a/gpmu_ucode.bin", "rb");
-	fd_data = fopen("/sys/class/firmware/gk20a!gpmu_ucode.bin/data", "wb");
-	while((c=getc(fd_firm))!=EOF)
-		fprintf(fd_data,"%c",c);
-
-	if (fclose(fd_firm)) ERROR("Error closing firmware file.\n");
-	if (fclose(fd_data)) ERROR("Error closing firmware sysfs file.\n");
-	
-	write(fd_loading, "0", 1);
-	close(fd_loading);
-	sleep(1); // wait on other firmware to load.
-	unlink("/etc");
-	umount("/system");
-	ERROR("Finished firmware load.\n");
-}
-
-void mrom_hook_before_fb_close() {
-	// Kill the extra threads
-	kill_pid(PID_TOUCH);
-	kill_pid(PID_WATCHDOG);
-}
-
-int mrom_hook_after_android_mounts(const char *busybox_path, const char *base_path, int type) {
-	mrom_hook_before_fb_close();
-	return 0;
-}
-
 void tramp_hook_before_device_init() {
+	FILE *fd_cmdline = NULL;
+	char *line = NULL;
+	size_t len = 0, read = 0;
+	int second_boot = 0;
+  
+	// If second boot, skip.
+	wait_for_file("/proc/cmdline", 10);
+	fd_cmdline = fopen("/proc/cmdline", "r");
+	if (fd_cmdline >= 0) {
+	  while ((read = getline(&line, &len, fd_cmdline)) != -1) {
+	    if (strstr(line, "mrom_kexecd=1")) {
+	      second_boot++;
+	      break;
+	    }
+	  }
+	  fclose(fd_cmdline);
+	  if (line)
+	    free(line);
+	  if (second_boot)
+	    return;
+	}
+
 	signal(SIGCHLD, SIG_IGN);
 
 	// Spawn touch thread
 	if (fork() == 0) {
-		write_pid(PID_TOUCH, getpid());
 		ts_thread();
 		_exit(0);
 	}
 
-	// Spawn thread to load firmware
+	// Load firmware thread
 	if (fork() == 0) {
 		load_firmware();
 		_exit(0);
@@ -120,3 +154,4 @@ void tramp_hook_before_device_init() {
 		_exit(0);
 	}
 }
+#endif
