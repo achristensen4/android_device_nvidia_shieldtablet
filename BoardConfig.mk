20232ce9a9819202f7cf4f7ebcc0982dc2b0d601
BitO BSessiOn <bitobsession@gmail.com>
2016-06-03 11:02:22 +0200

    Omni env

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,7 +31,7 @@ TARGET_NO_RADIOIMAGE := true
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/nvidia/shieldtablet
 TARGET_KERNEL_CONFIG := cyanogenmod_shieldtablet_defconfig
-BOARD_KERNEL_IMAGE_NAME := zImage-dtb
+BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_KERNEL_CMDLINE := androidboot.hardware=tn8 androidboot.selinux=permissive
 
 TARGET_USERIMAGES_USE_EXT4 := true
